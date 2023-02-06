fa2c10d51db49d913405abed3a8e95cfe67a3ed7
Naheen Habib <naheenhabib@outlook.com>
2023-02-06 20:54:52 +1100

    new commit

--- module3/passengerCounter/index.js
+++ module3/passengerCounter/index.js
@@ -1,12 +1,16 @@
 let count = 0
 
 let countEl = document.getElementById("count-el")
+let saveEl = document.getElementById("save-el")
 
 function increment() {
-    count = count + 1
-    countEl.innerText = count
+    count += 1
+    countEl.textContent = count
 }
 
 function save() {
-    console.log(count)
+    let saveNumber = count + "-" 
+    saveEl.textContent += saveNumber
+    countEl.textContent=0
+    count=0
 }

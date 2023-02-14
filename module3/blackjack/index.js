61e6eaf8f5a8244a14c15a8b5c674078e40a6898
Naheen Habib <naheenhabib36@gmail.com>
2023-02-14 19:01:13 +1100

    new commit

--- module3/blackjack/index.js
+++ module3/blackjack/index.js
@@ -4,6 +4,9 @@ let sum = firstCard + secondCard
 let hasBlackJack = false
 let isAlive = true
 let message = ""
+let messageEl=document.getElementById('message-el')
+
+function startGame(){
 
 if (sum <= 20) {
     message = ("Do you want to draw a new card? 🙂 ")
@@ -16,5 +19,7 @@ else {
     message = ("You're out of the game! 😭")
     isAlive = false
 }
+messageEl.textContent=message
+}
 
-console.log(message)
+

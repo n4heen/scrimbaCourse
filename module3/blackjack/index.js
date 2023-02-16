e80c6974ffe6d2320447980d6a8c7f296b283d9e
Naheen Habib <naheenhabib36@gmail.com>
2023-02-16 15:55:44 +1100

    new commit

--- module3/blackjack/index.js
+++ module3/blackjack/index.js
@@ -1,43 +1,50 @@
-// let firstCard = 10
-// let secondCard = 11
-// let cards = [firstCard, secondCard] 
-// let sum = firstCard + secondCard
-// let hasBlackJack = false
-// let isAlive = true
-// let message = ""
-// let messageEl = document.querySelector('#message-el')
-// let sumEl = document.querySelector('#sum-el')
-// let cardsEl = document.querySelector('#cards-el')
+let firstCard = getRandomCard()
+let secondCard = getRandomCard()
+let cards = [firstCard, secondCard]
+let sum = firstCard + secondCard
+let hasBlackJack = false
+let isAlive = true
+let message = ""
+let messageEl = document.querySelector('#message-el')
+let sumEl = document.querySelector('#sum-el')
+let cardsEl = document.querySelector('#cards-el')
 
-// function renderGame() {
+function getRandomCard() {
+  return  Math.floor(Math.random() * 50)
+}
 
-//     if (sum <= 20) {
-//         message = "Do you want to draw a new card? 🙂 "
-//     }
-//     else if (sum == 21) {
-//         message = "Wohoo! You've got Blackjack! 🥳 "
-//         hasBlackJack = true
-//     }
-//     else {
-//         message = "You're out of the game! 😭"
-//         isAlive = false
-//     }
-//     cardsEl.textContent = "Cards: " + cards[0] + " " + cards[1]
-//     sumEl.textContent = "Sum: " + sum
-//     messageEl.textContent = message
-// }
+function renderGame() {
 
-// function startGame(){
-//     renderGame()
-// }
+    if (sum <= 20) {
+        message = "Do you want to draw a new card? 🙂 "
+    }
+    else if (sum == 21) {
+        message = "Wohoo! You've got Blackjack! 🥳 "
+        hasBlackJack = true
+    }
+    else {
+        message = "You're out of the game! 😭"
+        isAlive = false
+    }
+    cardsEl.textContent = "Cards: "
+    sumEl.textContent = "Sum: " + sum
+    messageEl.textContent = message
+    for (let i = 0; i < cards.length; i++) {
+        cardsEl.textContent += cards[i] + ' '
+    }
+}
 
-// function newCard() {
-//     let card=7
-//     sum+=card
-//     cards.push(card)
-//     console.log(cards)
-//     startGame()
-// }
+function startGame() {
+    renderGame()
+}
+
+function newCard() {
+    let card = getRandomCard()
+    sum += card
+    cards.push(card)
+    console.log(cards)
+    startGame()
+}
 
 let sentence = ["Hello ", "my ", "name ", "is ", "Per"]
 let greetingEl = document.getElementById("greeting-el")

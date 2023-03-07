a0e0ad48f614ad49e28cb019042831f978c689e2
Naheen Habib <naheenhabib@outlook.com>
2023-03-07 22:20:41 +1100

    new commit

--- module3/blackjack/index.js
+++ module3/blackjack/index.js
@@ -1,15 +1,39 @@
 let firstCard = 10
-let secondCard = 7
+let secondCard = 8
+let cards = [firstCard, secondCard]
 let sum = firstCard + secondCard
 let hasBlackJack = false
+let isAlive = true
+let message = ""
+let messageEl = document.querySelector("#message-el")
+let sumEl = document.querySelector("#sum-el")
+let cardsEl = document.querySelector("#cards-el")
 
-if (sum <= 20) {
-    console.log("Do you want to draw a new card? 🙂")
+function startGame() {
+    renderGame()
 }
-else if (sum == 21) {
-    console.log("Wohoo! You've got Blackjack! 🥳")
-    hasBlackJack = true
+
+function renderGame() {
+    sumEl.textContent = "Sum: " + sum
+    cardsEl.textContent = "Cards: " + cards[0] + " " + cards[1]
+    if (sum <= 20) {
+        messageEl.textContent = "Do you want to draw a new card?"
+    }
+    else if (sum == 21) {
+        messageEl.textContent = "You've got Blackjack!"
+        hasBlackJack = true
+    }
+    else {
+        messageEl.textContent = "You're out of the game!"
+        isAlive = false
+
+    }
 }
-else {
-    console.log("You're out of the game! 😭")
+
+function newCard() {
+    let drawCard = 8
+    sum = sum + drawCard
+    renderGame()
 }
+
+

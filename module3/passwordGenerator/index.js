b047fad9dce0f54ed8def08f29e8a9db66a134aa
Naheen Habib <naheenhabib@outlook.com>
2023-03-18 17:31:36 +1100

    new commit

--- module3/passwordGenerator/index.js
+++ module3/passwordGenerator/index.js
@@ -1,6 +1,14 @@
-const characters = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z","a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z", "0", "1", "2", "3", "4", "5", "6", "7", "8", "9","~","`","!","@","#","$","%","^","&","*","(",")","_","-","+","=","{","[","}","]",",","|",":",";","<",">",".","?",
-"/"];
+const characters = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z", "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z", "0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "~", "`", "!", "@", "#", "$", "%", "^", "&", "*", "(", ")", "_", "-", "+", "=", "{", "[", "}", "]", ",", "|", ":", ";", "<", ">", ".", "?",
+    "/"];
 
+let password1El = document.querySelector(".password__1")
+let password2El = document.querySelector(".password__2")
+let generateButton = document.querySelector(".generateButton")
 
-
+function generate() {
+    let randomIndexOne = Math.floor(Math.random() * characters.length)
+    let randomIndexTwo = Math.floor(Math.random() * characters.length)
+    password1El.textContent = characters[randomIndexOne]
+    password2El.textContent = characters[randomIndexTwo]
+}
 

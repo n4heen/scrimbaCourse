c05c49a46f5b8583af9b06bd152623e0acbf7d96
Naheen Habib <naheenhabib@outlook.com>
2023-03-19 23:43:29 +1100

    new commit

--- module3/chromeExtension/index.js
+++ module3/chromeExtension/index.js
@@ -1,15 +1,23 @@
-let myLeads = ["www.awesomelead.com", "www.epiclead.com", "www.greatlead.com"]
+let myLeads = []
 let inputEl = document.querySelector('#input-el')
 let saveEl = document.querySelector('#input-btn')
 let ulEl = document.querySelector('#ul-el')
 
 saveEl.addEventListener("click", function () {
     myLeads.push(inputEl.value)
-
+    inputEl.value = ''
+    renderLeads()
 })
 
-for (let i = 0; i < myLeads.length; i++) {
-    ulEl.innerHTML += "<li>" + myLeads[i] + "</li>"
+function renderLeads() {
+    let listItems = ''
+    for (let i = 0; i < myLeads.length; i++) {
+        listItems += `
+        <li> 
+         <a href="${myLeads[i]}" target="_blank">${myLeads[i]}</a>
+          </li>
+          `
+    }
 
+    ulEl.innerHTML = listItems
 }
-

8a0154246be667759fd9014a0f30196115da2dce
Naheen Habib <naheenhabib@outlook.com>
2023-03-27 19:03:01 +1100

    new commit

--- module3/chromeExtension/index.js
+++ module3/chromeExtension/index.js
@@ -1,28 +1,39 @@
 let myLeads = []
-let inputEl = document.querySelector('#input-el')
-let saveEl = document.querySelector('#input-btn')
-let ulEl = document.querySelector('#ul-el')
+let oldLeads = []
+const inputEl = document.querySelector('#input-el')
+const inputBtn = document.querySelector('#input-btn')
+const ulEl = document.querySelector('#ul-el')
+const deleteBtn = document.querySelector('#delete-btn')
+const leadsFromLocalStorage = JSON.parse(localStorage.getItem('myLeads'))
 
-let leadsFromLocalStorage=localStorage.getItem('myLeads')
+if (leadsFromLocalStorage) {
+    myLeads = leadsFromLocalStorage
+    render(myLeads)
+}
 
-
-saveEl.addEventListener("click", function () {
-    myLeads.push(inputEl.value)
-    inputEl.value = ''
-    localStorage.setItem('myLeads', JSON.stringify(myLeads))
-    renderLeads()
-    console.log(leadsFromLocalStorage)
-})
-
-function renderLeads() {
+function render(leads) {
     let listItems = ''
-    for (let i = 0; i < myLeads.length; i++) {
+    for (let i = 0; i < leads.length; i++) {
         listItems += `
         <li> 
-         <a href="${myLeads[i]}" target="_blank">${myLeads[i]}</a>
+         <a href="${leads[i]}" target="_blank">${leads[i]}</a>
           </li>
           `
     }
-
     ulEl.innerHTML = listItems
 }
+
+deleteBtn.addEventListener("dblclick", function () {
+    localStorage.clear()
+    myLeads = []
+    render(myLeads)
+})
+
+inputBtn.addEventListener("click", function () {
+    myLeads.push(inputEl.value)
+    inputEl.value = ''
+    localStorage.setItem('myLeads', JSON.stringify(myLeads))
+    render(myLeads)
+
+    console.log(leadsFromLocalStorage)
+})

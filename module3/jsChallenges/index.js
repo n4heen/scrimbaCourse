639ec4977d5947586665d900705747f8649fd891
Naheen Habib <naheenhabib@outlook.com>
2023-04-01 14:55:21 +1100

    new commit

--- module3/jsChallenges/index.js
+++ module3/jsChallenges/index.js
@@ -1,10 +1,21 @@
 // Create a function that renders the three team images
 // Use a for loop, template strings (``), plus equals (+=)
 // .innerHTML to solve the challenge.
-
+const ulEl = document.querySelector('#container')
 const imgs = [
     "images/hip1.jpg",
     "images/hip2.jpg",
     "images/hip3.jpg"
 ]
 
+function render() {
+    let listItems = ''
+    for (let i = 0; i < imgs.length; i++) {
+        listItems +=
+            ` <img class="team-img" src="${img[i]}">
+           `
+    }
+    container.innerHTML = listItems
+}
+
+render()

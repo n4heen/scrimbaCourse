bb42784ca349b16540277cf51cbda260affa7f90
Naheen Habib <naheenhabib@outlook.com>
2023-04-08 17:41:36 +1000

    new commit

--- module3/emojis/index.js
+++ module3/emojis/index.js
@@ -1,1 +1,8 @@
+let emojiEl = document.getElementById('emojiContainer')
 const myEmojis = ["👨‍💻", "⛷", "🍲"]
+
+for (let i = 0; i < myEmojis.length; i++) {
+    console.log(myEmojis[i])
+}
+
+console.log(emojiEl)

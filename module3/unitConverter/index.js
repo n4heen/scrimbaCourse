3cfa7422d2814dc5fdb4830e6da354bdcebd54f0
Naheen Habib <naheenhabib@outlook.com>
2023-04-08 16:50:18 +1000

    new commit

--- module3/unitConverter/index.js
+++ module3/unitConverter/index.js
@@ -3,4 +3,18 @@
 1 liter = 0.264 gallon
 1 kilogram = 2.204 pound
 */
+const inputEl = document.querySelector('#input-el')
+const meterEl = document.querySelector('.meter')
+const literEl = document.querySelector('.liter')
+const kiloEl = document.querySelector('.kilo')
+
 console.log('egg')
+
+function convert() {
+    const meterShow = meterEl.val
+}
+
+function increment() {
+    count += 1
+    countEl.textContent = count
+}

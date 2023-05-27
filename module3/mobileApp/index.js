2bbc1d248c95e0b0fc4df1a45d7b8887f36de5ae
Naheen Habib <naheenhabib@outlook.com>
2023-05-27 19:19:06 +1000

    new commit

--- module3/mobileApp/index.js
+++ module3/mobileApp/index.js
@@ -1,1 +1,11 @@
-console.log('egg')
+
+const inputFieldEl = document.getElementById("input-field")
+const addButtonEl = document.getElementById("add-button")
+
+
+
+addButtonEl.addEventListener("click", addButton)
+
+function addButton() {
+    console.log('i')
+}
